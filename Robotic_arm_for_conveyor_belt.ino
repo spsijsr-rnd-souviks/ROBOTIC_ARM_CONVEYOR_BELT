75814e8ff4c69ceb6aad66f1070674b23ca49f89
spsijsr-rnd-souviks <souvik.santra@spsig.com>
2024-12-13 17:56:21 +0530

    Code refinement of system active function and adding some conditions

--- Robotic_arm_for_conveyor_belt.ino
+++ Robotic_arm_for_conveyor_belt.ino
@@ -14,11 +14,12 @@ ezButton button2(3);                                   // Button 2 for scooping 
 Servo servo1;                                          //Creating servo objects inside the class Servo
 Servo servo2;
 Servo servo3;
-Servo servo4;
+// Servo servo4;
 
   unsigned int count1 = 0;                              //Initializing count1 & count2 
   unsigned int count2 = 0;                                
 
+  int activAngles [4];
 
 void setup() {
   // put your setup code here, to run once:
@@ -38,12 +39,14 @@ void setup() {
   servo1.attach(5);                                      //Signal wire of servo-1 is connected to pin 5
   servo2.attach(6);                                      //Signal wire of servo-2 is connected to pin 6
   servo3.attach(9);                                      //Signal wire of servo-3 is connected to pin 9
-  servo4.attach(10);                                     //Signal wire of servo-4 is connected to pin 10
+//  servo4.attach(10);                                     //Signal wire of servo-4 is connected to pin 10
 
   int angle1 = 0;
   int angle2 = 0;
   int angle3 = 0;                                       // initializing servo angles
-  int angle4 = 0;
+//  int angle4 = 0;
+
+
 
   delay(1000);
   Serial.println("System initialized");
@@ -104,32 +107,41 @@ void loop() {
 
 void sysActiv ()
 {
-    if (systempark == true)
+    int max_pos_angle1 = 90;              // Defining experimented positions of the servos  
+    int max_pos_angle2 = 120;             // At the active position
+    int max_pos_angle3 = 105;             // Change here only if needed
+
+  // Checking servo positions if those are already in the active positions
+    if (angle1 != 90 || angle2 != 120 || angle3 != 105)     
     {
 
-        for (int b2 = 0; b2<= 90; b2++)
+        for (int b2 = angle2; b2 <= 90; b2++)
         {
           servo2.write(b2);
           delay(20);
         }
 
-        for (int b3 = 90; b3 >= 45; b3--)
+        for (int b3 = angle3; b3 >= max_pos_angle3 ; b3--)
         {
-          servo3.write(b3);
+          servo3.write(b3); 
           delay(20);
         }
 
-        for (int b2 = 90; b2<=135; b2++)
+        for (int b2 = 90; b2<=max_pos_angle2; b2++)
         {
           servo2.write(b2);
           delay(20);
         }
 
-        for (int b1 = 0; b1<= 90; b1++)
+        for (int b1 = angle1; b1 <= max_pos_angle1 ; b1++)
         {
           servo1.write(b1);
           delay(20);
         }
+
+      activAngles[0] = servo1.read();
+      activAngles[1] = servo2.read();
+      activAngles[2] = servo3.read();
     }
 
     else
@@ -180,16 +192,37 @@ void parking ()         //Call this function when system is going to park
 
 void scooping ()
   {
-    for
+    for (int c3 = a3; c<= 45; c3--)
     {
-      servo4.write(c4);
-      servo1.write(c1);
+      servo3.write(c3);
       delay(10);
     }
 
   }  
 
-void pour (int angle1, int angle2, int angle3, int angle4)
+void pour ()
   {
+    for (int d1=90; d1>=0; d1--)
+    {
+      servo1.write(d1);
+      delay(20);
+    }
+    for (int d2=120; d2>=90; d2--)
+    {
+      servo2.write(d2);
+      delay (10);
+    }
+    for (int d3=15; d3<= 105; d3--)
+    {
+      servo3.write(d3);
+    }
+    for (int d2= 90; d2<= 120; d2++)
+  }
 
+  void returnToActive ()
+  {
+    for (int q1=0; q1<=90; q1++ )
+    {
+      servo1.write(q1);
+    }
   }
