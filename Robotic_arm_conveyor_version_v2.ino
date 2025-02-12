6bf42480c1e16a07c10d82b7ddc8ae19ce7e012d
spsijsr-rnd-souviks <souvik.santra@spsig.com>
2025-02-12 15:56:08 +0530

    Display status with LCD

--- Robotic_arm_conveyor_version_v2.ino
+++ Robotic_arm_conveyor_version_v2.ino
@@ -1,17 +1,22 @@
 #include <Servo.h>
 #include <ezButton.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
 // Define buttons
 ezButton start_button(2);
 ezButton scoop_pour(3);
 ezButton stop_button(4);
 
+// set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2); 
+
 // Define servo motors
 Servo servo1; 
 Servo servo2; 
 Servo servo3;
 
-// Define states
+// Define states for state machine switches
 enum State {
               IDLE,
               ACTIVE,
@@ -20,11 +25,13 @@ enum State {
               PARKING
             };
 
+// Initialize state as IDLE
 State currentState = IDLE;
-unsigned long scoopingStartTime = 0;
-const long scoopingDuration = 5000; //  Scooping runs for 5 seconds
 
-// Servo angles
+//unsigned long scoopingStartTime = 0;
+//const long scoopingDuration = 5000; //  Scooping runs for 5 seconds
+
+// Experimented and tunned Servo angles
 int activ_angle1 = 140; 
 int activ_angle2 = 120; 
 int activ_angle3 = 60;
@@ -53,7 +60,13 @@ int curr_Angles[3] = {0, 0, 0};
 void setup() 
 {
     Serial.begin(9600);
+    lcd.init();         // initialize the lcd
+    lcd.backlight();    // Turn on the LCD screen backlight
     Serial.println("Power ON");
+    lcd.setCursor(1, 0);
+    lcd.print("Power ON");
+    delay(2000);
+    lcd.clear();
 
     start_button.setDebounceTime(50);
     scoop_pour.setDebounceTime(50);
@@ -65,15 +78,16 @@ void setup()
     servo2.write(park_angle2);
     servo3.attach(9);
     servo3.write(park_angle3);
-    
-    
-    
-  //  delay(500);
-
-
-
+  
     delay(1000);
+
     Serial.println("System initialized");
+    lcd.setCursor(2, 0);
+    lcd.print("System ");
+    lcd.setCursor(2, 1);
+    lcd.print("intialized");
+    delay(2000);
+    lcd.clear(); 
 }
 
 void loop() 
@@ -102,35 +116,74 @@ void loop()
     switch (currentState) 
     {
         case IDLE:
+
             Serial.println("System is READY...");
+            lcd.setCursor(0, 0);
+            lcd.print("System is Ready  ");
             break;
 
         case ACTIVE:
+
             Serial.println("System is ACTIVATED...");
+            lcd.setCursor(0, 0);
+            lcd.print("SYSTEM ACTIVATED");
             break;
 
         case SCOOPING:
+
             // Checks condition for start action
             if (material_weight < weight_threshold) 
             {
                 scooping();
                 Serial.print("Scooping complete! | ");
+
+            //  LCD display    
+                lcd.setCursor(0, 0);
+                lcd.print("Scooping");
+                lcd.setCursor(0, 1);
+                lcd.print("complete");
+               
                 material_weight += 5;
+
                 Serial.print("Item weight =");
                 Serial.println(material_weight);
+                
+            // LCD display    
+                lcd.setCursor(12, 0);
+                lcd.print("W:");
+                lcd.setCursor(14, 0);
+                lcd.print(material_weight);
+                lcd.setCursor(14, 1);
+                lcd.print("K");
+                delay(20);
+                //lcd.clear();
                 currentState = POURING;
             }             
             
             else
             {
               Serial.println("Threshold reached...");   // Prevents action if action button is pressed after it's complete
+              lcd.clear();
+              lcd.setCursor(0, 0);
+              lcd.print("Threshold ");
+              lcd.setCursor(0, 1);
+              lcd.print("reached");
+              delay(2000);
+              lcd.clear();
               currentState = ACTIVE; 
             }
 
             break;
 
         case POURING:
+
             Serial.println("Pouring material...");
+            lcd.setCursor(0, 0);
+            lcd.print("Pouring ");
+            lcd.setCursor(0, 1);
+            lcd.print("material ");
+            //delay(2000);
+            //lcd.clear();
             pour();
 //            material_weight += 5;
             delay(500);
@@ -153,6 +206,11 @@ void loop()
 
         case PARKING:
             Serial.println("Parking system...");
+            lcd.clear();
+            lcd.setCursor(0, 0);
+            lcd.print("Parking...");
+            //lcd.setCursor(2, 1);
+            //lcd.print("System");
             parking();
             systemActive = false;           // Reset active state flag
             material_weight = 0;            // Reset material weight for further scooping action
@@ -178,12 +236,28 @@ void scooping_Handler()
     if (!systemActive) 
     {
         Serial.println("ERROR: System is not active. Activate first!");
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("ERROR...");
+        delay(1000);
+        lcd.setCursor(0, 1);
+        lcd.print("Activate First..");
+        delay(1000);
+        lcd.clear();
+        //lcd.print("SYSTEM ACTIVATED")
         return;
     }
 
     if (currentState == ACTIVE) 
     {
         Serial.println("Scooping started...");
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Scooping");
+        lcd.setCursor(0, 1);
+        lcd.print("started");
+        //delay(2000);
+        //lcd.clear();
         currentState = SCOOPING;
     }
 }
@@ -193,6 +267,12 @@ void parking_Handler()
     if (currentState == SCOOPING || currentState == POURING) 
     {
         Serial.println("Finishing current task before parking...");
+        lcd.setCursor(2, 0);
+        lcd.print("Finishing current task");
+        lcd.setCursor(2, 1);
+        lcd.print("before parking");
+        delay(2000);
+        lcd.clear();
         return;
     }
     currentState = PARKING;
@@ -203,6 +283,13 @@ void parking_Handler()
 void sysActiv() 
 {
     Serial.println("Activating system...");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Activating");
+    lcd.setCursor(0, 1);
+    lcd.print("system...");
+    //delay(2000);
+    
     moveServo(servo2, curr_Angles[1], interim_angle2, 50);
     Serial.println("Servo2 responded");
     moveServo(servo3, curr_Angles[2], activ_angle3, 50);
@@ -215,11 +302,18 @@ void sysActiv()
     curr_Angles[0] = activ_angle1;
     curr_Angles[1] = activ_angle2;
     curr_Angles[2] = activ_angle3;
+    lcd.clear();
 }
 
 void parking() 
 {
     Serial.println("Parking system...");
+    //lcd.setCursor(2, 0);
+    //lcd.print("parking");
+    //lcd.setCursor(2, 1);
+    //lcd.print("system");
+    //delay(2000);
+    
     moveServo(servo1, curr_Angles[0], park_angle1, 50);
     Serial.println("Servo1 responded");
     moveServo(servo2, curr_Angles[1], interim_angle2, 30);
@@ -232,13 +326,24 @@ void parking()
     curr_Angles[0] = park_angle1;
     curr_Angles[1] = park_angle2;
     curr_Angles[2] = park_angle3;
+    //lcd.clear();
 }
 
 void scooping()
 {
+  lcd.setCursor(0, 0);
+  lcd.print("Scooping");
+  lcd.setCursor(0, 1);
+  lcd.print("Started...");
   moveServo(servo3, scoop_arm_angle_min, scoop_arm_angle_max, 50);
   Serial.println("Servo3 responded for scooping");
+  //lcd.setCursor(2, 0);
+  //lcd.print("Scooping");
+  //lcd.setCursor(2, 1);
+  //lcd.print("Activating");
+  //delay(2000);
   scoopingAction = true;
+  //lcd.clear();
 }
 
 void pour() 
@@ -246,10 +351,21 @@ void pour()
     if (!scoopingAction) 
     {
         Serial.println("No material to pour.");
+        lcd.setCursor(2, 0);
+        lcd.print("No material");
+        lcd.setCursor(2, 1);
+        lcd.print("to pour");
+        delay(2000);
+        lcd.clear();
         return;
     }
 
     Serial.println("Pouring process...");
+    //lcd.setCursor(2, 0);
+    //lcd.print("Pouring");
+    //lcd.setCursor(2, 1);
+    //lcd.print("process");
+    //delay(2000);
     moveServo(servo1, activ_angle1, park_angle1, 50);
     Serial.println("Servo1 responded for pour");
     moveServo(servo2, activ_angle2, interim_pour_angle2, 30);
@@ -257,10 +373,15 @@ void pour()
     Serial.println("Servo3 responded for pour");
     moveServo(servo2, interim_pour_angle2, activ_angle2, 50);
     Serial.println("Servo2 responded for pour");
-
+   //  lcd.clear();
     pouringStatus = true;
     Serial.println("Pouring complete.");
-
+    //lcd.setCursor(2, 0);
+    //lcd.print("Pouring ");
+    //lcd.setCursor(2, 1);
+    //lcd.print("Complete");
+    //delay(2000);
+    //lcd.clear();
     curr_Angles[1] = activ_angle2;
     curr_Angles[2] = scoop_arm_angle_min;
 }
@@ -271,6 +392,12 @@ void returnToActive()
     moveServo(servo3, curr_Angles[2], activ_angle3, 50);
     moveServo(servo1, park_angle1, activ_angle1, 50);
     Serial.println("System returned to ACTIVE...");
+    lcd.setCursor(0, 0);
+    lcd.print("System returened");
+    lcd.setCursor(0, 1);
+    lcd.print("to Active");
+    delay(2000);
+    lcd.clear();
 }
 
 // ======================== Function to move servo ========================
